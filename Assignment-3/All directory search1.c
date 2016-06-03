58e723b9a016596b40830152c4a3f15ec4a568cd
Yash Karle <yashkarle@gmail.com>
2016-06-03 11:08:44 +0530

    recursive directory search
    This program recursively searches all the files and folders present in the system

---
+++ Assignment-3/All directory search1.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+#include<string.h>
+
+void listdir(const char *name, int level)
+{
+    DIR *dir;
+    struct dirent *entry;
+
+    if (!(dir = opendir(name)))
+        return;
+    if (!(entry = readdir(dir)))
+        return;
+
+    do {
+        
+	if (entry->d_type == DT_DIR) 
+	{
+            char path[1024];
+
+            int len = snprintf(path, sizeof(path)-1, "%s/%s", name, entry->d_name);
+
+            path[len] = 0;
+
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+                continue;
+
+            printf("[%s]\n",entry->d_name);
+            listdir(path, level + 1);
+        }
+
+	  	else
+		{   
+        	    printf("\t%s\n",entry->d_name);
+		}
+    	} while (entry = readdir(dir));
+
+    closedir(dir);
+}
+
+int main(void)
+{
+    listdir("..", 0);
+    
+    return 0;
+}
